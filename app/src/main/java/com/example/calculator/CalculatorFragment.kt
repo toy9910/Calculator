70c40cf200db41e922501b341101652398c27c5c
toy9910 <50603217+toy9910@users.noreply.github.com>
2021-01-14 22:12:14 +0900

    제곱 기능 추가

--- app/src/main/java/com/example/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/example/calculator/CalculatorFragment.kt
@@ -1,10 +1,7 @@
 package com.example.calculator
 
 import android.os.Bundle
-<<<<<<< HEAD
-=======
 import android.util.Log
->>>>>>> d7b0ceb (제곱 기능 추가)
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,17 +10,10 @@ import kotlinx.android.synthetic.main.layout_calculator.*
 import kotlinx.android.synthetic.main.layout_calculator.view.*
 import java.lang.StringBuilder
 import java.util.*
-<<<<<<< HEAD
-
-class CalculatorFragment : Fragment(), View.OnClickListener {
-    var dataBuffer = StringBuilder()
-    lateinit var stringTokenizer : StringTokenizer
-=======
 import kotlin.math.pow
 
 class CalculatorFragment : Fragment(), View.OnClickListener {
     var dataBuffer = StringBuilder() // 숫자 저장 공간
->>>>>>> d7b0ceb (제곱 기능 추가)
     val stack = Stack<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,10 +45,7 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
         root.btn_erase.setOnClickListener(this)
         root.btn_erase_all.setOnClickListener(this)
         root.btn_result.setOnClickListener(this)
-<<<<<<< HEAD
-=======
         root.btn_square.setOnClickListener(this)
->>>>>>> d7b0ceb (제곱 기능 추가)
         return root
     }
 
@@ -67,10 +54,6 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
 
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> d7b0ceb (제곱 기능 추가)
     fun evaluate(stack: Stack<String>) : String? {
         var result : String? = null
         while(stack.size > 2) {
@@ -79,9 +62,6 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
             val operand1 = stack.pop().toDouble()
             when(operation) {
                 "+" -> {
-<<<<<<< HEAD
-=======
->>>>>>> d7b0ceb (제곱 기능 추가)
                     val result = operand1 + operand2
                     stack.push(result.toString())
                 }
@@ -91,23 +71,15 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 }
             }
         }
-<<<<<<< HEAD
-        result = stack.pop()
-=======
         result = stack.peek()
->>>>>>> d7b0ceb (제곱 기능 추가)
         return result
     }
 
     override fun onClick(view: View?) {
         when(view?.id) {
             R.id.btn_0 -> {
-<<<<<<< HEAD
-                if(tv_result.text != "") {
-=======
                 if(stack.size == 1) { // = 연산 이후 숫자를 누르면 식 초기화
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
@@ -115,12 +87,8 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 dataBuffer.append("0")
             }
             R.id.btn_00 -> {
-<<<<<<< HEAD
-                if(tv_result.text != "") {
-=======
                 if(stack.size == 1) {
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
@@ -128,12 +96,8 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 dataBuffer.append("00")
             }
             R.id.btn_1 -> {
-<<<<<<< HEAD
-                if(tv_result.text != "") {
-=======
                 if(stack.size == 1) {
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
@@ -141,12 +105,8 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 dataBuffer.append("1")
             }
             R.id.btn_2 -> {
-<<<<<<< HEAD
-                if(tv_result.text != "") {
-=======
                 if(stack.size == 1) {
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
@@ -154,12 +114,8 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 dataBuffer.append("2")
             }
             R.id.btn_3 -> {
-<<<<<<< HEAD
-                if(tv_result.text != "") {
-=======
                 if(stack.size == 1) {
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
@@ -167,12 +123,8 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 dataBuffer.append("3")
             }
             R.id.btn_4 -> {
-<<<<<<< HEAD
-                if(tv_result.text == "") {
-=======
                 if(stack.size == 1) {
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
@@ -180,12 +132,8 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 dataBuffer.append("4")
             }
             R.id.btn_5 -> {
-<<<<<<< HEAD
-                if(tv_result.text != "") {
-=======
                 if(stack.size == 1) {
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
@@ -193,12 +141,8 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 dataBuffer.append("5")
             }
             R.id.btn_6 -> {
-<<<<<<< HEAD
-                if(tv_result.text != "") {
-=======
                 if(stack.size == 1) {
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
@@ -206,12 +150,8 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 dataBuffer.append("6")
             }
             R.id.btn_7 -> {
-<<<<<<< HEAD
-                if(tv_result.text != "") {
-=======
                 if(stack.size == 1) {
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
@@ -219,12 +159,8 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 dataBuffer.append("7")
             }
             R.id.btn_8 -> {
-<<<<<<< HEAD
-                if(tv_result.text != "") {
-=======
                 if(stack.size == 1) {
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
@@ -232,12 +168,8 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 dataBuffer.append("8")
             }
             R.id.btn_9 -> {
-<<<<<<< HEAD
-                if(tv_result.text != "") {
-=======
                 if(stack.size == 1) {
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
@@ -245,31 +177,14 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 dataBuffer.append("9")
             }
             R.id.btn_point -> {
-<<<<<<< HEAD
-                if(tv_result.text != "") {
-=======
                 if(stack.size == 1) {
                     stack.clear()
->>>>>>> d7b0ceb (제곱 기능 추가)
                     tv_result.setText("")
                     formula.setText("")
                 }
                 formula.append(".")
                 dataBuffer.append(".")
             }
-<<<<<<< HEAD
-            R.id.btn_add -> {
-                var data = ""
-                if(dataBuffer.isEmpty())
-                        data = stack.pop()
-                else
-                    data = dataBuffer.toString()
-
-                if(stack.size > 0) {
-                    val operation = stack.pop()
-                    when(operation) {
-                        "*" -> {
-=======
             R.id.btn_add -> { // + 연산
                 var data = ""
                 if(dataBuffer.isEmpty()) { // = 계산 후 이어서 계산할 때
@@ -283,34 +198,22 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                     val operation = stack.pop() // + 앞 연산자
                     when(operation) {
                         "*" -> { // 해당 연산자의 앞 연산자가 * 일 경우 예) 2*2+ , 4*3+ , ...
->>>>>>> d7b0ceb (제곱 기능 추가)
                             val operand2 = data.toDouble()
                             val operand1 = stack.pop().toDouble()
                             val result = operand1 * operand2
                             stack.push(result.toString())
                         }
-<<<<<<< HEAD
-                        "/" -> {
-=======
                         "/" -> { // 해당 연산자의 앞 연산자가 / 일 경우 예) 8/2+ , 10/5+ , ...
->>>>>>> d7b0ceb (제곱 기능 추가)
                             val operand2 = data.toDouble()
                             val operand1 = stack.pop().toDouble()
                             val result = operand1 / operand2
                             stack.push(result.toString())
                         }
-<<<<<<< HEAD
-                        "-" -> {
-=======
                         "-" -> { // 해당 연산자의 앞 연산자가 - 일 경우 예) 8-2+ , 9-1+ , ...
->>>>>>> d7b0ceb (제곱 기능 추가)
                             var data2 = "-$data"
                             stack.push("+")
                             stack.push(data2)
                         }
-<<<<<<< HEAD
-                        else -> {
-=======
                         "^" -> { // 해당 연산자의 앞 연산자가 ^ 일 경우 예) 2^3+ , 2+5^2+ , ...
                             val operand2 = data.toDouble()
                             val operand1 = stack.pop().toDouble()
@@ -343,35 +246,18 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                             }
                         }
                         else -> { // 해당 연산자의 앞 연산자가 + 일 경우 예) 8+2+ , 10+5+ , ...
->>>>>>> d7b0ceb (제곱 기능 추가)
                             stack.push(operation)
                             stack.push(data)
                         }
                     }
                 }
-<<<<<<< HEAD
-                else {
-=======
                 else { // 식의 맨 처음 부분을 입력할 경우
->>>>>>> d7b0ceb (제곱 기능 추가)
                     stack.push(data)
                 }
                 stack.push("+")
                 formula.append("+")
                 dataBuffer.delete(0,dataBuffer.length)
             }
-<<<<<<< HEAD
-            R.id.btn_subtract -> {
-                var data = ""
-                if(dataBuffer.isEmpty())
-                    data = stack.pop()
-                else
-                    data = dataBuffer.toString()
-                if(stack.size > 0) {
-                    val operation = stack.pop()
-                    when(operation) {
-                        "*" -> {
-=======
             R.id.btn_subtract -> { // - 연산
                 var data = ""
                 if(dataBuffer.isEmpty()) // = 계산 후 이어서 계산할 때
@@ -391,36 +277,17 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                             stack.push(data2)
                         }
                         "*" -> { // 해당 연산자의 앞 연산자가 * 일 경우 예) 8*2- , 10*5- , ...
->>>>>>> d7b0ceb (제곱 기능 추가)
                             val operand2 = data.toDouble()
                             val operand1 = stack.pop().toDouble()
                             val result = operand1 * operand2
                             stack.push(result.toString())
                         }
-<<<<<<< HEAD
-                        "/" -> {
-=======
                         "/" -> { // 해당 연산자의 앞 연산자가 / 일 경우 예) 8/2- , 10/5- , ...
->>>>>>> d7b0ceb (제곱 기능 추가)
                             val operand2 = data.toDouble()
                             val operand1 = stack.pop().toDouble()
                             val result = operand1 / operand2
                             stack.push(result.toString())
                         }
-<<<<<<< HEAD
-                        "-" -> {
-                            var data2 = "-$data"
-                            stack.push("+")
-                            stack.push(data2)
-                        }
-                        else -> {
-                            stack.push(operation)
-                            stack.push(data)
-                        }
-                    }
-                }
-                else {
-=======
                         "^" -> { // 해당 연산자의 앞 연산자가 ^ 일 경우 예) 2^3- , 2+5^2- , ...
                             val operand2 = data.toDouble()
                             val operand1 = stack.pop().toDouble()
@@ -455,25 +322,12 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                     }
                 }
                 else { // 식의 맨 처음 부분을 입력할 경우
->>>>>>> d7b0ceb (제곱 기능 추가)
                     stack.push(data)
                 }
                 stack.push("-")
                 formula.append("-")
                 dataBuffer.delete(0,dataBuffer.length)
             }
-<<<<<<< HEAD
-            R.id.btn_multiply -> {
-                var data = ""
-                if(dataBuffer.isEmpty())
-                    data = stack.pop()
-                else
-                    data = dataBuffer.toString()
-                if(!stack.empty()) {
-                    val operation = stack.pop()
-                    when(operation) {
-                        "-" -> {
-=======
             R.id.btn_multiply -> { // * 연산
                 var data = ""
                 if(dataBuffer.isEmpty()) // = 계산 후 이어서 계산할 때
@@ -488,20 +342,10 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                             stack.push(data)
                         }
                         "-" -> { // 해당 연산자의 앞 연산자가 - 일 경우 예) 8-2* , 9-1* , ...
->>>>>>> d7b0ceb (제곱 기능 추가)
                             var data2 = "-$data"
                             stack.push("+")
                             stack.push(data2)
                         }
-<<<<<<< HEAD
-                        else -> {
-                            stack.push(operation)
-                            stack.push(data)
-                        }
-                    }
-                }
-                else {
-=======
                         "*" -> { // 해당 연산자의 앞 연산자가 * 일 경우 예) 8*2* , 10*5* , ...
                             val operand2 = data.toDouble()
                             val operand1 = stack.pop().toDouble()
@@ -548,25 +392,12 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                     }
                 }
                 else { // 식의 맨 처음 부분을 입력할 경우
->>>>>>> d7b0ceb (제곱 기능 추가)
                     stack.push(data)
                 }
                 formula.append("*")
                 dataBuffer.delete(0,dataBuffer.length)
                 stack.push("*")
             }
-<<<<<<< HEAD
-            R.id.btn_divide -> {
-                var data = ""
-                if(dataBuffer.isEmpty())
-                    data = stack.pop()
-                else
-                    data = dataBuffer.toString()
-                if(!stack.empty()) {
-                    val operation = stack.pop()
-                    when(operation) {
-                        "-" -> {
-=======
             R.id.btn_divide -> { // / 연산
                 var data = ""
                 if(dataBuffer.isEmpty()) // = 계산 후 이어서 계산할 때
@@ -581,20 +412,10 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                             stack.push(data)
                         }
                         "-" -> { // / 앞의 연산자가 - 일 경우 예) 8-2/ , 9-1/ , ...
->>>>>>> d7b0ceb (제곱 기능 추가)
                             var data2 = "-$data"
                             stack.push("+")
                             stack.push(data2)
                         }
-<<<<<<< HEAD
-                        else -> {
-                            stack.push(operation)
-                            stack.push(data)
-                        }
-                    }
-                }
-                else {
-=======
                         "*" -> { // / 앞의 연산자가 * 일 경우 예) 8*2/ , 10*5/ , ...
                             val operand2 = data.toDouble()
                             val operand1 = stack.pop().toDouble()
@@ -641,37 +462,12 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                     }
                 }
                 else { // 식의 맨 처음 부분을 입력할 경우
->>>>>>> d7b0ceb (제곱 기능 추가)
                     stack.push(data)
                 }
                 formula.append("/")
                 dataBuffer.delete(0,dataBuffer.length)
                 stack.push("/")
             }
-<<<<<<< HEAD
-            R.id.btn_erase -> {
-                if(dataBuffer.isEmpty())
-                    stack.pop()
-                else
-                    dataBuffer.delete(dataBuffer.length - 1, dataBuffer.length)
-                val text_formula = StringBuilder(formula.text)
-                text_formula.delete(text_formula.length-1,text_formula.length)
-                formula.text = text_formula.toString()
-            }
-            R.id.btn_erase_all -> {
-                formula.setText("")
-                tv_result.setText("")
-                dataBuffer.delete(0,dataBuffer.length)
-                while(!stack.empty()) {
-                    stack.pop()
-                }
-            }
-            R.id.btn_result -> {
-                var data = dataBuffer.toString()
-                val operation = stack.pop()
-                when(operation) {
-                    "*" -> {
-=======
             R.id.btn_erase -> { // 한 칸 지우기
                 if(formula.text == ""){}
                 else {
@@ -695,25 +491,17 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                 val operation = stack.pop() // = 바로 앞 연산자
                 when(operation) { // 마지막 연산 계산
                     "*" -> { // 마지막 연산이 * 일 경우  예) 2+1+4*3= , 3+5*2= , ...
->>>>>>> d7b0ceb (제곱 기능 추가)
                         val operand2 = data.toDouble()
                         val operand1 = stack.pop().toDouble()
                         val result = operand1 * operand2
                         stack.push(result.toString())
                     }
-<<<<<<< HEAD
-                    "/" -> {
-=======
                     "/" -> { // 마지막 연산이 / 일 경우  예) 2+1+4/2= , 3+6/2= , ...
->>>>>>> d7b0ceb (제곱 기능 추가)
                         val operand2 = data.toDouble()
                         val operand1 = stack.pop().toDouble()
                         val result = operand1 / operand2
                         stack.push(result.toString())
                     }
-<<<<<<< HEAD
-                    else -> {
-=======
                     "^" -> { // 마지막 연산이 ^ 일 경우  예) 8+2^3= , 4^2= , ...
                         val operand2 = data.toDouble()
                         val operand1 = stack.pop().toDouble()
@@ -743,22 +531,12 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
                         }
                     }
                     else -> { // 마지막 연산이 + 또는 - 일 경우  예) 8+2+3= , 4-2= , ...
->>>>>>> d7b0ceb (제곱 기능 추가)
                         stack.push(operation)
                         stack.push(data)
                     }
                 }
                 tv_result.setText(evaluate(stack))
                 dataBuffer.delete(0,dataBuffer.length)
-<<<<<<< HEAD
-                stack.clear()
-            }
-        }
-
-    }
-
-
-=======
             }
             R.id.btn_square -> { // ^ 연산
                 var data = ""
@@ -802,5 +580,4 @@ class CalculatorFragment : Fragment(), View.OnClickListener {
             }
         }
     }
->>>>>>> d7b0ceb (제곱 기능 추가)
 }
